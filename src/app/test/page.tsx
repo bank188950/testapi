766335dde1364c9d32c9ed63640516a253174140
bankkub <panupong.som@sansiri.com>
2024-02-15 13:57:46 +0700

    custom scrollbar

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -4,6 +4,8 @@ import React, { useEffect, useState } from "react";
 import Pa from "@/components/pa";
 import Show from "@/components/show";
 import { useQuery } from "react-query";
+import "mac-scrollbar/dist/mac-scrollbar.css";
+import { MacScrollbar } from "mac-scrollbar";
 
 export default function Test() {
   const { data, isLoading, isError } = useQuery({
@@ -38,6 +40,49 @@ export default function Test() {
       <br />
       <br />
       <Pa />
+      <MacScrollbar
+        minThumbSize={50}
+        className="h-48 w-30"
+        // suppressAutoHide={true}
+        suppressScrollX={true}
+        thumbStyle={function () {
+          const thumbStyle: React.CSSProperties = {
+            backgroundColor: "red",
+            // borderRadius: "10px",
+          };
+          return thumbStyle;
+        }}
+        trackStyle={function () {
+          const trackStyle: React.CSSProperties = {
+            backgroundColor: "blue",
+            width: "10px",
+          };
+          return trackStyle;
+        }}
+        trackGap={50}
+      >
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+        <div>Content</div>
+      </MacScrollbar>
     </div>
   );
 }
